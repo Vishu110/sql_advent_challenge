7575307ff1c22584167121d8f1bfd8707b9ddee1
Vishal US <vishalus1997@gmail.com>
2026-01-09 19:25:20 +0530

    day-15 sql challenge

---
+++ sql-advent-calendar-vishalus1997/day-15.sql
@@ -0,0 +1,22 @@
+-- SQL Advent Calendar - Day 15
+-- Title: The Grinch's Mischief Tracker
+-- Difficulty: hard
+--
+-- Question:
+-- The Grinch is tracking his daily mischief scores to see how his behavior changes over time. Can you find how many points his score increased or decreased each day compared to the previous day?
+--
+-- The Grinch is tracking his daily mischief scores to see how his behavior changes over time. Can you find how many points his score increased or decreased each day compared to the previous day?
+--
+
+-- Table Schema:
+-- Table: grinch_mischief_log
+--   log_date: DATE
+--   mischief_score: INTEGER
+--
+
+-- My Solution:
+
+select *,
+        lag(mischief_score) over(order by log_date) lag_score,
+        (mischief_score - lag(mischief_score) over(order by log_date)) score_diff
+from grinch_mischief_log
